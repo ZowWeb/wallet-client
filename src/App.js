53eba7ef3813d40a90fb82006b7d929f8842caa9
ZowWeb <zohaibandkhan@gmail.com>
2021-09-07 18:51:31 +0530

    :tada: Initial commit - Install antd - Setup ESLint - Cleanup files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Empty } from 'antd'
+
+import 'antd/dist/antd.css'
+import './App.css'
+
+const { Header, Footer, Content } = Layout
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Header>Header</Header>
+      <Content className="site-layout-content">
+        <Empty description="No content yet..." />
+      </Content>
+      <Footer>Zohaib Khan ©2021</Footer>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
